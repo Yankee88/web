94cb27a66a4305004d86b81e18868c8397aca581
KromberK123 <a37507687@gmail.com>
2022-10-05 20:42:21 +0700

    gg

---
+++ lesson_1/main.py
@@ -0,0 +1,45 @@
+# x = "Программирование"
+# y = x[-1]
+# print(x.index(y) + 1)
+# name_1 = "Антон"
+# name_2 = "Богдан"
+# name_3 = "Петя"
+# bratva = [name_1, name_2, name_3]
+# print(bratva)
+# print(bratva[0])
+# print(bratva[1][2:]) #bratva[1]-anton,2 - срез
+# x = "c:/Python3/python.exe"
+# print("Имя файла:",x[11:] )
+# print("Расширение:",x[-3:] )
+# print("Имя каталога:",x[3:10] )
+# print("Полный путь к каталогу:",x[:10] )
+# x = "c:/Python3/python.exe"
+# temp = x.split("/")
+# print(temp)
+# print("Имя файла:",temp[-1] )
+# print("Расширение:",temp[-1][-3:])
+# print("Имя каталога:",temp[1] )
+# print("Полный путь к каталогу:",temp[0] + "/" + temp[1] )
+# chapter_1 = input("Глава 1:")
+# page = input("Страница:")
+# chapter_2 = input("Глава 2:")
+# page_2 = input("Страница:")
+# chapter_3 = input("Глава 3:")
+# page_3 = input("Страница:")
+# print(chapter_1.ljust(15),page.rjust(15))
+# print(chapter_2.ljust(15),page_2.rjust(15))
+# print(chapter_3.ljust(15),page_3.rjust(15))
+# x = "123456789"
+# print(x[::2])  #через один
+# print(x[::-1]) #в обратном порядке
+# print([x::-2]) #в обратном порядке через один
+# str = "12'345'678"
+# temp = str[:2] + str[3:5] + str[-3:]
+# number = int(temp)
+# print(number)
+# temp = str.split("'")
+# temp_2 = temp[0] + temp[1] + temp[2]
+# number = int(temp_2)
+# print(number)
+# y = str.replace("'", "")
+# print(y)
